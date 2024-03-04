5b35fbe49538b8556a1e28d4d5346b8da61e481f
youdilab <youdilabnotes@gmail.com>
2024-03-05 00:35:22 +0530

    Stack and heap memory allocation demo.

--- CppLearn/CppLearn.cpp
+++ CppLearn/CppLearn.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
 
+class YoudiClass
+{
+public:
+    int index;
+    std::string name;
+};
+
 int main()
 {
     std::cout << "C++ Learning\n";
+
+    std::cout << "Memory Allocation\n";
+
+    //Allocating on stack
+    YoudiClass* prolongedStack;
+    {
+        YoudiClass onStack = YoudiClass();
+        onStack.index = 0;
+        onStack.name = "Stack";
+        prolongedStack = &onStack;
+    }
+
+    std::cout << "Name of Variable on stack: " + prolongedStack->name + "\n";
+
+    //Allocating on heap
+    YoudiClass* prolongedHeap;
+    {
+        YoudiClass* onHeap = new YoudiClass();
+        onHeap->index = 1;
+        onHeap->name = "Heap";
+        prolongedHeap = onHeap;
+    }
+
+    std::cout << "Name of Variable on heap: " + prolongedHeap->name + "\n";
+    delete prolongedHeap;//Making sure the heap allocated object is deleted.
 }
