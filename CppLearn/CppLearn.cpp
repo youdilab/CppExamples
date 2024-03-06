01f7932206d12e690c58f67361cfd492e64a91eb
youdilab <youdilabnotes@gmail.com>
2024-03-06 15:52:21 +0530

    Implicit conversion and explicit keyword demo.

--- CppLearn/CppLearn.cpp
+++ CppLearn/CppLearn.cpp
@@ -7,6 +7,22 @@ public:
     std::string name;
 };
 
+class Person
+{
+private:
+    std::string m_name;
+    int m_age;
+public:
+    Person(const std::string name) : m_name(name), m_age(-1) {}
+    //explicit Person(int age) : m_name("Unknown"), m_age(age) {}
+    Person(int age) : m_name("Unknown"), m_age(age) {}
+};
+
+void PrintPerson(const Person& person)
+{
+    std::cout << "Printing Person\n";
+}
+
 int main()
 {
     std::cout << "C++ Learning\n";
@@ -56,6 +72,25 @@ int main()
 
     c->~int();//destructor
     delete b;//deallocating memory
+
+
+    //Implicit Conversion and Explicit Keyword---------------------------
+    std::cout << "\n\nImplicit Conversion and Explicit Keyword\n\n";    
+
+    //Stadard way to create objects.
+    Person person1("A");
+    Person person2(25);
+
+    //Implicit conversion--
+    Person person3 = 26;//C++ calls the relevant constructor which takes in an int
+    Person person4 = std::string("B");
+
+    //Passing a constructor to a function parameter instead of a Person object.
+    PrintPerson(27);
+    PrintPerson(std::string("C"));
+
+    //"Explicit" keyword disables this functionality.
+    //It is a keyword used in fromt of a constructor.
 }
    
 
