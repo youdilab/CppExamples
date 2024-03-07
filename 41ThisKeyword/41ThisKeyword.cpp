c59b1a017b331c55113fc0402a100fff5add7e8c
youdilab <youdilabnotes@gmail.com>
2024-03-08 00:28:01 +0530

    41 Use of this keyword part II

--- 41ThisKeyword/41ThisKeyword.cpp
+++ 41ThisKeyword/41ThisKeyword.cpp
@@ -8,17 +8,19 @@ class Youdi2DVector
 public:
     Youdi2DVector(int x, int y)
     {
-        this->x = x;
+        this->x = x;//this keyword distinguishes between object's x vs incoming x.
         this->y = y;
 
         Youdi2DVector* const dummy = this;
     }
 
+    //This can be a const function as it does not change class properties.
     int getX() const
     {
         return x;
     }
 
+    //This can be a const function as it does not change class properties.
     int getY() const
     {
         return y;
@@ -29,7 +31,7 @@ private:
     int y;    
 };
 
-void print2dVector(const Youdi2DVector& myVector)
+void print2dVector(Youdi2DVector& myVector)
 {
     std::cout << myVector.getX();
     std::cout << myVector.getY();
@@ -38,5 +40,5 @@ void print2dVector(const Youdi2DVector& myVector)
 int main()
 {
     Youdi2DVector one{ 2, 3 };
+    print2dVector(one);
 }
-
